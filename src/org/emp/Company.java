ed5f7369642f22329aa1140590742e3b06480024
NitishGreens <nitishgreens@gmail.com>
2021-10-29 13:07:33 +0530

    updated

---
+++ src/org/emp/Company.java
@@ -0,0 +1,13 @@
+package org.emp;
+
+public class Company {
+
+	public static void main(String[] args) {
+		System.out.println("welcome");
+		System.out.println("welc");
+		System.out.println("welce");
+		System.out.println("welome");
+		System.out.println("wecome");
+	}
+
+}
